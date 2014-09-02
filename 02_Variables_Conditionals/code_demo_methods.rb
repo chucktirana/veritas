1adde86e723d062e98662f5fb67744146d8dbdaa
Chuck Tirana <charlestiranabarry@gmail.com>
2014-09-02 19:07:38 -0400

    booleans and methods

--- 02_Variables_Conditionals/code_demo_methods.rb
+++ 02_Variables_Conditionals/code_demo_methods.rb
@@ -7,12 +7,15 @@
 #1 - Let's say we wanted to perform the task of multiplying 22 by 44  
 ###########
 ###########
+puts (22 * 44)
 	
 #2 - Great, what if we wanted to multiply 22*4 again. If we had to type that out each time, it would be inefficient
 # So, let's try and save that code to a method...
 ###########
 ###########
-
+def multiply 
+	22 * 44
+end
 
 
 # NOTE: Running this does not return 968! How come?
@@ -21,7 +24,7 @@
 ##3 - Let's call that method, by simply typing out its name:
 ###########
 ###########
-
+puts multiply
 
 
 ##4 - Now, let's say we wanted to make it a little smarter. Our  method will take two parameters passed to it.
@@ -30,14 +33,20 @@
 ###########
 ###########
 
+def add_numbers(num1, num2)
+	puts num1 + num2
+end
 
+add_numbers(1200, 1300)
 
 #5 - Now CALL that method:
 ###########
 ###########
+def multiply_numbers(num1, num2)
+	puts num1 * num2
+end
 
-
-
+multiply_numbers(1000000000000000, 1)
 #6 - Other People's methods. Two of the most common methods we will use throughout this class are puts and gets.
 # Let's start with puts...
 ###########
