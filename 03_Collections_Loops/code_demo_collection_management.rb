81a503043b6fbdb6946001f864d101ee5dbe9903
Max Miller <maxwellgmiller@gmail.com>
2014-09-09 17:39:28 -0400

    Lesson 3

---
+++ 03_Collections_Loops/code_demo_collection_management.rb
@@ -0,0 +1,34 @@
+#Arrays and Hashes
+#TIME: 25 min
+#INSTRUCTIONAL DESIGN NOTES:
+# => You can change the values in the hashes or arrays. Make it personal to you.
+# => Make sure to emphasize that a hash is a dictionary (e.g. phone book) so students can relate
+# => Provide this file (minus these instructions) to students so they can follow along
+#and focus on logic and understanding rather than stress over syntax.
+# => In class labs for hashes are a stepping stone to using JSON returned string from the Mashable and Reddit in the Teddit app.
+
+
+
+# Print the name of each of the 3 stooges
+# If you call `puts` without any parameters, it will output an empty line
+# Use that to space out your output nicely
+
+
+#
+# This works with Hashes as well
+
+
+# What about an array of hashes?
+
+# Explain how the `each do` syntax is the same as `each {`
+# (i.e. at a high level, explain that blocks can be {} or do/end)
+
+
+# What about a hash with an array as a value?
+# Notice the formatting
+# Explain that ruby 2.0 syntax is OPTIONAL and the string key syntax
+# will not go away anytime soon
+
+
+# Start with a collection that looks like our Twitter response. I've kept only the parts we care about.
+
