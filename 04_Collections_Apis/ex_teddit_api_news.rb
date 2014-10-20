26d8d7390fab3c3c0679b611a71d33d42a6eb295
Chuck Tirana <charlestiranabarry@gmail.com>
2014-10-20 19:56:22 -0400

    Adding lots of stufff

--- 04_Collections_Apis/ex_teddit_api_news.rb
+++ 04_Collections_Apis/ex_teddit_api_news.rb
@@ -5,3 +5,20 @@
 
 # Pull the json, parse it and then make a new story hash out of each story(Title, Category, Upvotes)
 # Add each story to an array and display your "Front page"
+
+require 'net/http'
+require "json"
+data = Net::HTTP.get("digg.com","/api/news/popular.json")
+hash = JSON.parse(data)
+stories = hash['data']['feed']
+story_array = []
+stories.each do |story|
+	title = story['content']['title']
+	url = story['content']['url']
+	diggs = story ['diggs']['count']
+	description = story ['content']['description']
+	tags = story ['content']['tags'].map{|tag| tag['display']}
+	story_hash = {'title'=>title, 'description'=> description, 'url'=> url,'diggs'=>diggs, 'tags'=>tags}
+	story_array << story_hash
+end
+puts story_array
